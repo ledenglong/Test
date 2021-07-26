9c37a53c65251c8ceaa64235e017fcbf46a9d4fb
ldl <2325790567@qq.com>
2021-07-26 19:48:35 +0800

    first commit

---
+++ test04/src/main/java/com/ldl/crud/bean/Emp.java
@@ -0,0 +1,102 @@
+package com.ldl.crud.bean;
+
+public class Emp {
+    private Integer empId;
+
+    private String empName;
+
+    private String gender;
+
+    private String email;
+
+    private Integer dId;
+
+    private String dName;
+
+    public Emp(String empName, String gender, String email, Integer dId) {
+        this.empName = empName;
+        this.gender = gender;
+        this.email = email;
+        this.dId = dId;
+    }
+
+    public Emp(Integer empId, String empName, String gender, String email, Integer dId, String dName) {
+        this.empId = empId;
+        this.empName = empName;
+        this.gender = gender;
+        this.email = email;
+        this.dId = dId;
+        this.dName = dName;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public Emp() {
+    }
+
+    public Emp(Integer empId, String empName, String gender, String email, Integer dId) {
+        this.empId = empId;
+        this.empName = empName;
+        this.gender = gender;
+        this.email = email;
+        this.dId = dId;
+    }
+
+    public Integer getEmpId() {
+        return empId;
+    }
+
+    public void setEmpId(Integer empId) {
+        this.empId = empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getdId() {
+        return dId;
+    }
+
+    public void setdId(Integer dId) {
+        this.dId = dId;
+    }
+
+    @Override
+    public String toString() {
+        return "Emp{" +
+                "empId=" + empId +
+                ", empName='" + empName + '\'' +
+                ", gender='" + gender + '\'' +
+                ", email='" + email + '\'' +
+                ", dId=" + dId +
+                ", dName='" + dName + '\'' +
+                '}';
+    }
+}
